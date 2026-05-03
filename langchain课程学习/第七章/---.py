21d81f50cb9375a580ccb2c3f7b5ff65efd3b0a5
alvarezaidynl24524-arch <alvarezaidynl24524@gmail.com>
2026-05-03 22:39:02 +0800

    Delete langchain课程学习 directory

--- langchain课程学习/第七章/---.py
+++
@@ -1,5 +0,0 @@
-try:
-    from langchain_classic.tools.retriever import create_retriever_tool
-    print("✨ 终于找到了！就在 langchain_classic.tools.retriever")
-except ImportError:
-    print("❌ 路径 E 也失败了")
